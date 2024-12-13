3959c0aabb755952c03b6338af7ca95536c8b896
Mohamed Elhanchir <elhanchirmohamed349@gmail.com>
2024-12-13 16:13:26 +0100

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,16 +1,36 @@
 import strategy.IStrategy;
 import strategy.IStrategyImpl1;
 import strategy.IStrategyImpl2;
+import strategy.IStrategyImplByDefault;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
          Context context = new Context();
-         context.process();
-        System.out.println("____________________________");
-        IStrategy strategy = new IStrategyImpl2();
-         context.setStrategy(strategy);
-         context.process();
-        System.out.println("____________________________");
+         boolean finish = false;
+        Scanner scanner = new Scanner(System.in);
+            while (!finish) {
+                System.out.println("Enter the Startegy you want to apply: ");
+                String strategyClassName = scanner.nextLine();
+                IStrategy strategy = null;
+                try {
+                    strategy = (IStrategy) Class.forName(strategyClassName).getDeclaredConstructor().newInstance();
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException |
+                         InvocationTargetException e) {
+                    System.out.println("Strategy not found, applying default strategy.");
+                    strategy = new IStrategyImplByDefault();
+                }
+                context.setStrategy(strategy);
+                context.process();
+                System.out.println("________________________________________________________");
+                System.out.println("Do you want to continue? (Y/N)");
+                String continueChoice = scanner.nextLine();
+                if (continueChoice.equalsIgnoreCase("N")) {
+                    finish = true;
+                }
+            }
 
     }
 }
