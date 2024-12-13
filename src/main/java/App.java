662786a3f21743dc4a8b18739d56b0d0b6ce22bb
Mohamed Elhanchir <elhanchirmohamed349@gmail.com>
2024-12-13 18:22:09 +0100

    Cache with hashMap

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,23 +4,32 @@ import strategy.IStrategyImpl2;
 import strategy.IStrategyImplByDefault;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception{
-         Context context = new Context();
-         boolean finish = false;
+        Context context = new Context();
+        boolean finish = false;
         Scanner scanner = new Scanner(System.in);
+
+        HashMap<String, IStrategy> strategies = new HashMap<>();
+        IStrategy strategy = null;
             while (!finish) {
                 System.out.println("Enter the Startegy you want to apply: ");
                 String strategyClassName = scanner.nextLine();
-                IStrategy strategy = null;
-                try {
-                    strategy = (IStrategy) Class.forName(strategyClassName).getDeclaredConstructor().newInstance();
-                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException |
-                         InvocationTargetException e) {
-                    System.out.println("Strategy not found, applying default strategy.");
-                    strategy = new IStrategyImplByDefault();
+
+                strategy = strategies.get(strategyClassName);
+                if (strategy == null) {
+                    try {
+                        strategy = (IStrategy) Class.forName(strategyClassName).getDeclaredConstructor().newInstance();
+                        strategies.put(strategyClassName, strategy);
+                        System.out.println("Creating new strategy: " + strategyClassName);
+                    } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                             NoSuchMethodException | InvocationTargetException e) {
+                        System.out.println("Strategy not found, applying default strategy.");
+                        strategy = strategies.computeIfAbsent("default", k -> new IStrategyImplByDefault());
+                    }
                 }
                 context.setStrategy(strategy);
                 context.process();
